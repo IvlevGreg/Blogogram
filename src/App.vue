33d4c9dbb7010c021a881d638f57650e78c32707
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-01-11 23:18:47 +0300

    feat: create template

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post">
+    <h2 class="title">Пост о js</h2>
+    <p class="descr">1</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
