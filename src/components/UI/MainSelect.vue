3ff815eb610ce6af36599c6d8c29417c6f478dbc
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-01-14 15:32:03 +0300

    feat: create MainSelectComponent

---
+++ src/components/UI/MainSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <select name="" id="" class="select" :v-model="modelValue" @change="changeOption">
+    <option disabled value="chose" class="option">Выберите из списка</option>
+    <option class="option" v-for="option in options" :value="option.value" :key="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'main-select',
+  props: {
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  methods: {
+    changeOption(e) {
+      this.$emit('update:modelValue', e.target.value)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.select {
+  padding: 5px 10px;
+}
+.option {
+  padding: 5px;
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,5 +2,6 @@ import MainButton from '@/components/UI/MainButton.vue'
 import MainInput from '@/components/UI/MainInput.vue'
 import PopUp from '@/components/UI/PopUp.vue'
 import SpinnerTriple from '@/components/UI/SpinnerTriple.vue'
+import MainSelect from '@/components/UI/MainSelect.vue'
 
-export default [MainButton, MainInput, PopUp, SpinnerTriple]
+export default [MainButton, MainInput, PopUp, SpinnerTriple, MainSelect]
