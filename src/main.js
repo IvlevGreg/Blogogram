a96d6bf7992321af92b9c314e6cdafda9c51bc00
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-04-15 12:12:27 +0300

    feat: create store

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App'
 
 import componentsUi from '@/components/UI/index'
 import router from '@/router/router'
+import directives from '@/directives/index'
+import store from '@/store'
 
 const app = createApp(App)
 
@@ -10,4 +12,6 @@ componentsUi.forEach((component) => {
   app.component(component.name, component)
 })
 
-app.use(router).mount('#app')
+directives.forEach((directiveEl) => app.directive(directiveEl.name, directiveEl))
+
+app.use(router).use(store).mount('#app')
